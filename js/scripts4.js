058b6b7dbc2a410e71ea18a41f46f5f94ee6d278
Tony Carbetta <tony@Tonys-MacBook-Pro.local>
2018-04-25 20:09:52 -0700

    resized

--- js/scripts4.js
+++ js/scripts4.js
@@ -14,48 +14,48 @@ function printComment() {
         }
   // If user did not leave a field blank run this code
     else {
-      var getCommentSection = document.getElementById('comments');
-
-      getCommentSection.className = 'border border-dark border-rounded m-2 mb-5 mt-5';
+      //create new div to wrap each individual user/comment
+      var newDiv = document.createElement('div');
+      document.body.appendChild(newDiv);
+      newDiv.className = "col-md-offest-6 mx-auto max-height min-height max-width bg-light m-5 pl-4 pr-4 pt-4 border border-dark rounded";
       //create html to be written with user's input
       var usernameValueText = document.innerHTML = ('<b>Username:</b> <br>' + usernameValue);
-      var commentValueText = document.innerHTML = ('<b>Comment:</b> <br>' + commentValue);
+      usernameValueText.className = 'border-bottom';
+      var commentValueText = document.innerHTML = (commentValue);
+      commentValueText.className = 'border-bottom';
       //check if html is written correctly
       console.log(usernameValueText);
       console.log(commentValueText);
       //create new element for user's name
-      var printUsername = document.createElement('p');
+      var printUsername = document.createElement('h6');
       printUsername.innerHTML = usernameValueText;
-      printUsername.className = 'm-4';
+      printUsername.className = 'm-4 border border-bottom text-center border-rounded';
       // add new element to the Center of the page
-      getCommentSection.appendChild(printUsername);
+      newDiv.appendChild(printUsername);
       //create new element for user's comment
-      var printComment = document.createElement('p');
+      var printComment = document.createElement('h6');
       printComment.innerHTML = commentValueText;
-      printComment.className = 'm-4';
+      printComment.className = 'mt-2 border-dark comment-max-height overflow';
       // add new element to the Center of the page
-      getCommentSection.appendChild(printComment);
+      newDiv.appendChild(printComment);
       // var resetPlaceHolder = document.getElementById('username').placeholder = 'Username:';
       var getMyForm = document.getElementById('myForm');
       getMyForm.reset();
       //create the remove button
-    //   var button = document.createElement('button');
-    //   getCommentSection.appendChild(button);
-    //   button.className = 'btn btn-sm btn-dark border border-rounded text-light ml-5 mt-2 mb-4 text-center';
-    //   button.textContent = 'Delete';
-    //   var removeComment = function removeComment () {
-    //
-    //   };
-    //   button.onclick(removeComment);
-    //
-    // }
-}
+      var newDiv1 = document.createElement('div');
+      newDiv.appendChild(newDiv1);
+      // var button = document.createElement('button');
+      // newDiv1.appendChild(button);
+      // button.className = 'btn btn-lg active btn-dark border border-rounded text-light ml-5 mt-1 mb-2 text-center remove';
+      // button.textContent = 'Delete';
+
+
+
+
+    }
   }
-  validateForm();
-
-
+validateForm();
 }
-
 
 // THIS IS WORKING! KEEP IT! FOR REFERENCE
 // var newHeader = document.createElement('h3');
